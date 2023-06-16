218db2f4f8886a53041b95119279fb72da500b53
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-06-16 15:47:20 +0300

    refactor server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from 'express'
-const port = 5000
 import dotenv from 'dotenv'
 
-// dotenv.config()
+dotenv.config()
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Server is ready"))
-
-app.listen(port, () => console.log(`server stared on port ${port}`))
+app.get('/', (req, res) => {res.send('API is running....');
+});
+app.listen(port, () => console.log(`Server started on port ${port}`));
